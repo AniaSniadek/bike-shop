2e4d72f3d9d8d5308b0107bb863a7bc5bfff09b2
Speedy09 <sniadek97@gmail.com>
2020-03-20 17:38:09 +0100

    adding Model

--- src/Model.java
+++ src/Model.java
@@ -1,2 +1,13 @@
 public class Model {
+    private String model;
+
+    public Model() {}
+
+    public Model(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
 }
