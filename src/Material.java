5c4d706e508c4391a490690538c7823f8425f5e2
Speedy09 <sniadek97@gmail.com>
2020-03-20 17:39:24 +0100

    adding Material

--- src/Material.java
+++ src/Material.java
@@ -1,2 +1,13 @@
 public class Material {
+    private String material;
+
+    public Material() {}
+
+    public Material(String material) {
+        this.material = material;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
 }
