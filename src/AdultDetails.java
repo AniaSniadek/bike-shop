d40b277a730757ef8b058f780afe3c51d88e60d4
Speedy09 <sniadek97@gmail.com>
2020-03-21 10:31:53 +0100

    adding adult details class

---
+++ src/AdultDetails.java
@@ -0,0 +1,9 @@
+public class AdultDetails {
+    public BikeDetails bikeDetails;
+
+    public AdultDetails() {};
+
+    public AdultDetails(BikeDetails bikeDetails) {
+        this.bikeDetails = bikeDetails;
+    }
+}
