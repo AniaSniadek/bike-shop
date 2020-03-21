a9b6ead51c0e3bf97eb847d60bafaf2f67c5dcab
Speedy09 <sniadek97@gmail.com>
2020-03-21 13:03:29 +0100

    creating adults bikes

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,16 @@ public class Main {
 
         //creating products
         Adult bike1 = new Adult("M945", 980.00, prod1, "Navigto", type1, mat1);
+        Adult bike2 = new Adult("G570", 799.00, prod2, "Kim", type1, mat2);
+        Adult bike3 = new Adult("P456", 459.00, prod1, "Jasen", type2, mat2);
+        Adult bike4 = new Adult("S909", 1000.00, prod2, "Olsen", type3, mat3);
+        Adult bike5 = new Adult("F538", 999.00, prod1, "Bravo", type3, mat1);
 
         Child childBike1 = new Child("P456", 459.00, prod1, "Kido", type1, mat1, 2);
+        Child childBike2 = new Child("P456", 459.00, prod1, "Kido", type1, mat1, 2);
+        Child childBike3 = new Child("P456", 459.00, prod1, "Kido", type1, mat1, 2);
+        Child childBike4 = new Child("P456", 459.00, prod1, "Kido", type1, mat1, 2);
+        Child childBike5 = new Child("P456", 459.00, prod1, "Kido", type1, mat1, 2);
 
         //adding products to the Magazine
         Magazine magazine = new Magazine();
