56d07d48289f941dc8f858a5f15b7e8669b7a9df
Speedy09 <sniadek97@gmail.com>
2020-03-21 12:30:55 +0100

    preparing the store enviroment

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,22 @@ public class Main {
         Type type2 = new Type("bicyclist");
         Type type3 = new Type("mountain");
 
+        Material mat1 = new Material("aluminium");
+        Material mat2 = new Material("steel");
+        Material mat3 = new Material("carbon");
 
+        //creating products
+        AdultDetails det1 = new AdultDetails(prod1, "Navigto", type1, mat1);
+        Adult bike1 = new Adult("M945", 980.00, det1);
 
+        ChildDetails childDet1 = new ChildDetails(prod1, "Kido", type1, mat1, 2);
+        Child childBike1 = new Child("P456", 459.00, childDet1);
+
+        //adding products to the Magazine
+        Magazine magazine = new Magazine();
+        magazine.addBike(bike1);
+        magazine.addBike(childBike1);
+
+        magazine.printBikes();
     }
 }
