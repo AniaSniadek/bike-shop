bba81c6f37cf775dca8d485c3425606e6c7d6986
Speedy09 <sniadek97@gmail.com>
2020-03-21 10:31:41 +0100

    adding adult class

---
+++ src/Adult.java
@@ -0,0 +1,9 @@
+public class Adult {
+    public Bike bike;
+    public AdultDetails adultDetails;
+
+    public Adult(Bike bike, AdultDetails adultDetails) {
+        this.bike = bike;
+        this.adultDetails = adultDetails;
+    }
+}
