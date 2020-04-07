498be900f1231d46f174e68c3c5bbc7d3f7b3270
Speedy09 <sniadek97@gmail.com>
2020-04-07 10:44:57 +0200

    finish

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    //colors of text
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
 
